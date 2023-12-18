9b8ba3d2a67376ca86edc22f657ee3092664a62a
Jake Jackson <142851548+JakebJackson@users.noreply.github.com>
2023-12-18 23:12:11 +1100

    Added basic HTML, outlined necessary steps in comments

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Create question array with 4 multiple choice options for each question
+
+// Create timer function
+
+// Create randomiser for choosing questions (be sure to include logic for removing already selected questions until reset)
+
+// Create score storage function (LocalStorage), needs to allow user to input their name.
+
+// Create clear highscores logic
+
+// Create gameover/gamestart functions
+
