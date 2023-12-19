29f8fef78a59311df9bea540848853f647b6c624
Jake Jackson <142851548+JakebJackson@users.noreply.github.com>
2023-12-19 13:43:52 +1100

    Added reset.css, completed CSS & HTML.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,83 @@
+// Determining variables for project.
+var contentQuestionEl = document.getElementById("content-question");
+var questionListEl = document.getElementById("question-option");
+var titleEl = document.getElementById("quiz-title")
+
+// Question Objects for the quiz.
+var question1 = {
+    q: "What is the most common web based programming language?",
+    answer: "JavaScript",
+    option1: "C#",
+    option2: "Java",
+    option3: "C++",
+};
+var question2 = {
+    q: "",
+    answer: "",
+    option1: "",
+    option2: "",
+    option3: "",
+};
+var question3 = {
+    q: "",
+    answer: "",
+    option1: "",
+    option2: "",
+    option3: "",
+};
+var question4 = {
+    q: "",
+    answer: "",
+    option1: "",
+    option2: "",
+    option3: "",
+};
+var question5 = {
+    q: "",
+    answer: "",
+    option1: "",
+    option2: "",
+    option3: "",
+};
+var question6 = {
+    q: "",
+    answer: "",
+    option1: "",
+    option2: "",
+    option3: "",
+};
+var question7 = {
+    q: "",
+    answer: "",
+    option1: "",
+    option2: "",
+    option3: "",
+};
+var question8 = {
+    q: "",
+    answer: "",
+    option1: "",
+    option2: "",
+    option3: "",
+};
+var question9 = {
+    q: "",
+    answer: "",
+    option1: "",
+    option2: "",
+    option3: "",
+};
+var question10 = {
+    q: "",
+    answer: "",
+    option1: "",
+    option2: "",
+    option3: "",
+};
+
+console.log(question1.q);
+
+
 // Create question array with 4 multiple choice options for each question
 
 // Create timer function
@@ -10,3 +90,4 @@
 
 // Create gameover/gamestart functions
 
+// if (event.target == questionCurrent.answer) then change button background color to green, play win sound effect, score++ else change btn bgc to red, play loss sound effect.
